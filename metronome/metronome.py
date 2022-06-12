dae98d5a42155bcccca1fe6d46cb8f226910e334
Warren Fletcher <warren@fletch.band>
2022-06-12 19:16:51 +0200

    Super Basic Metronome

--- metronome/metronome.py
+++ metronome/metronome.py
@@ -1,5 +1,17 @@
 import pika
-
+import time
+import playsound
 # Let's do this simple at first
 
-time = [None] * 64 # Breaking down the metronome into small units of time
+time_grid = [None] * 64 # Breaking down the metronome into small units of time
+for i in range(0, len(time_grid), 1):
+    if (i%8) == 0:
+        time_grid[i] = 1
+
+index = 0
+while True:
+    index+=1
+    if time_grid[index%len(time_grid)]:
+        playsound.playsound("./samples/metronome.mp3")
+    time.sleep(0.125)
+
