5681fe6f05eae61fefd40e543dee930a09446e37
Warren Fletcher <warren@fletch.band>
2022-06-13 14:01:45 +0200

    Started writting metronome in go

--- metronome/metronome.go
+++ metronome/metronome.go
@@ -4,5 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Why hello Mr Fletcher.")
-
+	grid := make([]uint16, 32)
+	for i := 0; i < 32; i++ {
+		if i%8 == 0 {
+			grid[i] = 2
+		}
+		if i%8 == 0 {
+			grid[i] = 1
+		}
+	}
+	fmt.Printf("Metronome Configuratio: %d \n", grid)
 }
