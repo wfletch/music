ccc9bd6396945b8d4138dfb8af355065a0a3c281
Warren Fletcher <warren@fletch.band>
2022-06-13 13:46:51 +0200

    Added Softer Offbeat sound effect

--- metronome/metronome.py
+++ metronome/metronome.py
@@ -6,12 +6,18 @@ import playsound
 time_grid = [None] * 64 # Breaking down the metronome into small units of time
 for i in range(0, len(time_grid), 1):
     if (i%8) == 0:
+        time_grid[i] = 2
+    if (i%32) == 0:
         time_grid[i] = 1
 
 index = 0
+print(time_grid)
 while True:
+    if time_grid[index%len(time_grid)] == 1:
+        playsound.playsound("./samples/metronome.mp3", False)
+    elif time_grid[index%len(time_grid)] == 2:
+        playsound.playsound("./samples/metronome_soft.mp3", False)
     index+=1
-    if time_grid[index%len(time_grid)]:
-        playsound.playsound("./samples/metronome.mp3")
     time.sleep(0.125/4)
+    
 
